6c075f9fca7a7ecfafff23a1d4888b142d33acaa
ARCTICKING <arcticarc@edgerunner.com>
2025-01-29 22:25:23 +0530

    Vite conf update

--- vite.config.js
+++ vite.config.js
@@ -6,20 +6,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/localhost.pem')),
-    },
-    cors: {
-      origin: 'http://127.0.0.1',
-    },
-    host: "0.0.0.0"
-  },
-  optimizeDeps: {
-    exclude: ['bcrypt']
-  },
-  commonjsOptions: {
-    transformMeta: true
-  }
 });
